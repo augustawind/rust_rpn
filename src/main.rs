6f999842970d974f20a7e9604293d8da865b30c5
Dustin Rohde <dustin.rohde@gmail.com>
2017-02-26 16:36:04 -0500

    Got basic calculator working.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+type Stack = Vec<f64>;
+
+fn binop<F>(stack: &mut Stack, f: F) -> f64
+    where F: Fn(f64, f64) -> f64 {
+
+    if let (Some(x), Some(y)) = (stack.pop(), stack.pop()) {
+        f(x, y)
+    } else {
+        panic!("Not enough values on the stack.")
+    }
+}
+
+fn rpn(glyphs: Vec<&str>) -> f64 {
+    let mut stack: Stack = Vec::new();
+
+    for glyph in glyphs.into_iter() {
+        match glyph {
+            "+" => {
+                let val = binop(&mut stack, |x, y| x + y);
+                stack.push(val);
+            },
+            "-" => {
+                let val = binop(&mut stack, |x, y| x - y);
+                stack.push(val);
+            }
+            n   => match n.parse::<f64>() {
+                Ok(n) => stack.push(n),
+                Err(_) => panic!("Invalid glyph: {}", n),
+            },
+        }
+    }
+
+    stack[0]
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = rpn(vec!["5", "3", "+"]);
+    println!("Result: {}", result);
 }
